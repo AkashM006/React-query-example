9c5ed5801158142b500211c8cc06fb527ae9e3ff
Akash M <akashm006@outlook.com>
2023-06-12 17:02:31 +0530

    Implemented infinite scrolling

--- src/components/Home/CardList.tsx
+++ src/components/Home/CardList.tsx
@@ -1,7 +1,6 @@
 import Card from "./Card";
 import "./CardList.scss";
-import useNotes from "../../hooks/useNotes";
-import { useState } from "react";
+import useNotesInfinite from "../../hooks/useNotesInfinite";
 
 function CardList() {
   return (
@@ -13,44 +12,87 @@ function CardList() {
 }
 
 const QueryHandler = () => {
-  const [page, setPage] = useState<number>(1);
+  const {
+    data,
+    isLoading,
+    error,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useNotesInfinite();
 
-  const notesQuery = useNotes({ page });
+  if (isLoading) return <h1>Loading...</h1>;
 
-  const futureNotes = useNotes({ page: page + 1 });
-
-  const { status, data, error, isFetching } = notesQuery;
-
-  if (status === "error") {
-    return <h3>{error.response?.data?.msg}</h3>;
-  } else if (status === "loading") return <h3>Loading...</h3>;
+  console.log("Has next: ", { hasNextPage, isFetchingNextPage });
 
   return (
     <div className="cardlist__container">
-      {isFetching && (
-        <div className="overlay">
-          <p>Loading...</p>
+      {data?.pages.map((group) => {
+        return group.data.map((note) => (
+          <Card
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            body={note.body}
+          />
+        ));
+      })}
+      {hasNextPage && (
+        <div className="button__container">
+          <button
+            disabled={isFetchingNextPage}
+            onClick={() => {
+              console.log("here");
+              fetchNextPage();
+            }}
+          >
+            {isFetchingNextPage ? "Loading..." : "Load more"}
+          </button>
         </div>
       )}
-      <div className="top__button__container">
-        <button
-          disabled={page == 1}
-          onClick={() => setPage((prev) => prev - 1)}
-        >
-          Previous
-        </button>
-        <button
-          disabled={futureNotes.data && futureNotes.data.length === 0}
-          onClick={() => setPage((prev) => prev + 1)}
-        >
-          Next
-        </button>
-      </div>
-      {data.map((note) => (
-        <Card key={note.id} id={note.id} title={note.title} body={note.body} />
-      ))}
     </div>
   );
 };
+
+// const QueryHandler = () => {
+//   const [page, setPage] = useState<number>(1);
+
+//   const notesQuery = useNotes({ page });
+
+//   const futureNotes = useNotes({ page: page + 1 });
+
+//   const { status, data, error, isFetching } = notesQuery;
+
+//   if (status === "error") {
+//     return <h3>{error.response?.data?.msg}</h3>;
+//   } else if (status === "loading") return <h3>Loading...</h3>;
+
+//   return (
+//     <div className="cardlist__container">
+//       {isFetching && (
+//         <div className="overlay">
+//           <p>Loading...</p>
+//         </div>
+//       )}
+//       <div className="top__button__container">
+//         <button
+//           disabled={page == 1}
+//           onClick={() => setPage((prev) => prev - 1)}
+//         >
+//           Previous
+//         </button>
+//         <button
+//           disabled={futureNotes.data && futureNotes.data.length === 0}
+//           onClick={() => setPage((prev) => prev + 1)}
+//         >
+//           Next
+//         </button>
+//       </div>
+//       {data.map((note) => (
+//         <Card key={note.id} id={note.id} title={note.title} body={note.body} />
+//       ))}
+//     </div>
+//   );
+// };
 
 export default CardList;
---
+++ src/hooks/useNotesInfinite.tsx
@@ -0,0 +1,28 @@
+import {
+  UseInfiniteQueryResult,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import { getNotes } from "../api/notes";
+import { Note } from "../types/data";
+
+type List = {
+  count: number;
+  data: Note[];
+};
+
+function useNotesInfinite(): UseInfiniteQueryResult<List> {
+  return useInfiniteQuery(
+    ["notes"],
+    ({ pageParam = 1 }) => getNotes(pageParam),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (pages[0].count >= pages.length * 5) {
+          return pages.length + 1;
+        }
+        return undefined;
+      },
+    }
+  );
+}
+
+export default useNotesInfinite;
